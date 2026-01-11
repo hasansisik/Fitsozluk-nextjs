5286f8d372f2a4464ad94d5d1129166736ea9a3f
Hasan Şişik <sisik.hsn@gmail.com>
2026-01-12 02:07:10 +0300

    x

--- config.ts
+++ config.ts
@@ -1,3 +1,3 @@
-export const server = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3040/v1";
+export const server = process.env.NEXT_PUBLIC_API_URL || "https://fitsozluk-server-seven.vercel.app/v1";
 export const fitmailApiUrl = "https://api.fitmail.com";
 
